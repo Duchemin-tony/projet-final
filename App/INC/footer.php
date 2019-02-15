8ae8cf40e65c93a8a9a95d528a48127fe0a08272
Duchemin <tonyduchemin@MacBook-Pro-de-tony.local>
2019-02-15 11:26:38 +0100

    Ajout header et footer

---
+++ App/INC/footer.php
@@ -0,0 +1,9 @@
+<footer>
+	<div class=" container clearfix">
+		<p>Copyright © 2019 Blog de Jean Forteroche - Tous droits réservés - Réalisé par Duchemin Tony</p>			
+	</div> 
+</footer>
+
+</div> 
+</body>
+</html>
---
+++ App/INC/header.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  	<title>Développement Personnel</title>
+  	<meta name="description" content="">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<link rel="stylesheet" href="../../public/css/style.css">
+  	<link rel="stylesheet" href="../../public/css/main.css">
+</head>
+	<body>	
+
+		<div class="wrap">	
+			<div id="nav"> 
+    			<div class="container clearfix">
+		
+					<div id="navigation">
+						<ul id="menu" class="menu">
+							<li ><a href="index.php">Accueil</a></li>
+							<li ><a href="articles.php">Blog</a></li>
+							<li ><a href="contact.php">Contact</a></li>
+						</ul>	
+
+				<div class="membres">
+					<ul>
+						<li><a href="login.php?logout">Se déconnecter</a></li>
+						<li><a href="compte.php">Mon compte</a></li>
+						<li><a href="admin/">Administration</a></li>
+						<li><a href="login.php">Se connecter</a></li>
+						<li><a href="signup.php">S'inscrire</a></li>
+					</ul>
+				</div>		
+			</div> 
+    </div> 
+	</div> 
+
+	<header>
+	    <div class="container clearfix">
+
+			<div id="logo">
+				<a href="index.php" title="Développement personnel"><img  src="../../public/img/logo.png" alt="logo" /></a>
+			</div>
+			<div id="reseaux">
+				<ul>
+					<li><a href="fb" class="fb"></a></li>
+					<li><a href="twr" class="tt"></a></li>
+					<li><a href="gg" class="gg"></a></li>
+					<li><a href="yt" class="yt"></a></li>
+				</ul>
+			</div>		
+	    </div>
+	</header>
